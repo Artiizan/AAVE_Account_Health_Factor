b6c4ca4cbec446f005e2d2b322a98cd5e2002176
Brandon Grant <>
2021-06-01 08:14:42 +0000

    Formats Health Factor as done in AAVE

--- index.js
+++ index.js
@@ -19,14 +19,17 @@ var aaveContract = new ethers.Contract(contractAddress, abi, provider);
 
 // Getting the current Block
 provider.getBlockNumber().then(function(blockNumber) {
-	console.log('wallet address: ' + userAddress);
-	console.log('block number:   ' + blockNumber);
+	console.log('wallet address:    ' + userAddress);
+	console.log('block number:      ' + blockNumber);
 });
 
 // Getting the Account Information
 aaveContract.getUserAccountData(userAddress).then(function(userAccountData) {
 	var healthFactorHex = userAccountData['healthFactor']['_hex'];
-	console.log('health factor:  ' + BigInt(healthFactorHex));
+	var healthFactorWei = BigInt(healthFactorHex);
+	console.log('health factor hex: ' + healthFactorHex);
+	console.log('health factor wei: ' + healthFactorWei);
+	console.log('health factor:     ' + ethers.utils.formatEther(healthFactorWei));
 	if (healthFactorHex == '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff') {
 		console.log("PLEASE NOTE: Your health factor is the maximum uint256 value because you have not borrowed any coins from AAVE.")
 		// https://github.com/aave/aave-protocol/blob/master/contracts/lendingpool/LendingPoolDataProvider.sol#L328
